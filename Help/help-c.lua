332cd213053ef481bcb8a95cf167368ea3b08202
Blauw <95681031+BlauweEboy@users.noreply.github.com>
2022-01-19 10:34:44 +0100

    V1.0
    The first release, read the readme.md for more info

---
+++ Help/help-c.lua
@@ -0,0 +1,14 @@
+---                |
+---                V This is the command that you have to type ingame to acces the command. You have to type a / in front of it to. 
+RegisterCommand("help", function()
+    msg("Here you type your tekst to help players with commands/keybinds")
+    msg("Here you type even more of your tekst to help players with commands/keybinds")
+    --- For a new line of text in the command, replace this or enter a new line with msg("(YOUR TEXT)")
+end, false)
+
+function msg(text)
+    TriggerEvent("chatMessage", "[Your server name]", {255,0,0}, text)
+end
+
+--- How this looks ingame: [Your server here] Here you type your tekst to help players with commands/keybinds
+--- second line:           [Your server here] Here you type even more of your tekst to help players with commands/keybinds
