57d7a1c8fe53305bc2b073d7074b627c2ea437f8
Blauw <95681031+BlauweEboy@users.noreply.github.com>
2022-01-19 10:25:31 +0100

    Delete __resource.lua

--- Help/__resource.lua
+++
@@ -1,4 +0,0 @@
-resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
-
---- in the quotation marks after client_script you can edit the file name of the code. WARNING! DONT DO THIS IF YOU DONT KNOW WHAT YOU ARE DOING!
-client_script "help-c.lua"
